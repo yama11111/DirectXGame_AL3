ff253b6cfb9de47b63ef3200aaedc4ed8c67c0f2
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-06-13 13:59:57 +0900

    拡大まで

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -32,20 +32,24 @@ void GameScene::Initialize() {
 
 	worldTransform_.scale_ = {5.0, 1.0, 1.0};
 	
-	//Matrix4 matScale;
-	//matScale.m[0][0] = worldTransform_.scale_.x;
-	//matScale.m[1][1] = worldTransform_.scale_.y;
-	//matScale.m[2][2] = worldTransform_.scale_.z;
-	//matScale.m[3][3] = 1.0;
+	Matrix4 matScale;
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1.0;
 
-	//worldTransform_.matWorld_ = {
-	//	1, 0, 0, 0, 
-	//	0, 1, 0, 0, 
-	//	0, 0, 1, 0, 
-	//	0, 0, 0, 1
-	//};
+	worldTransform_.matWorld_ = {
+		1, 0, 0, 0, 
+		0, 1, 0, 0, 
+		0, 0, 1, 0, 
+		0, 0, 0, 1
+	};
 
-	//worldTransform_.matWorld_ = MultMatrix4(worldTransform_.matWorld_, matScale);
+	worldTransform_.matWorld_ = MultMatrix4(worldTransform_.matWorld_, matScale);
+
+	//worldTransform_.rotation_ = {0.0f, 0.0f, };
+	Matrix4 matRotZ;
+
 
 	worldTransform_.TransferMatrix();
 
