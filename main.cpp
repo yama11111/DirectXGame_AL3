b55e838df04f195797becc6f6d314e9f0b57a118
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-08-01 13:06:04 +0900

    02_04_ex3

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("LE2A_ヤマナカ_ルイ_AL3");
+	win->CreateGameWindow("LE2A_23_ヤマナカ_ルイ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
